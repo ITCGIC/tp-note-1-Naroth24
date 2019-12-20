1a7a32322f85f8b9ff1831f3041fe14ddf6806b1
Naroth24 <kunvannaroth@gmail.com>
2019-12-20 13:55:34 +0700

    t1

---
+++ src/Task1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task1{
+    public static void pr(String s){
+    System.out.println(s);
+}
+    public static void main(String[] args){
+    pr("**************************");
+    pr("*                      * *");
+    pr("*                   *    *");
+    pr("*                *       *");
+    pr("*             *          *");
+    pr("*          *             *");
+    pr("*       *                *");
+    pr("*    *                   *");
+    pr("* *                      *");
+    pr("**************************");
+    
+    }
+}
